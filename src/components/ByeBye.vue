5106580e1510f7497d7a911eabd4e7fa17096383
Tuan Duong <tuanduong15@gmail.com>
2017-02-02 18:36:20 -0800

    Initial Commit

---
+++ src/components/ByeBye.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<h5>Bye Bye</h5>
+		<div>This is some random ass text</div>
+		<div v-for="name in groupNames">
+			<div @click="runThisFunction">{{ name }}</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'ByeBye',
+		props: ["groupNames"],
+		methods: {
+			runThisFunction() {
+				console.log("your mama")
+			}
+		},
+		created() {
+		  console.log("Bye Bye component just got created")
+		}
+	}
+</script>
+
